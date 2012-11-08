316c86b6787f61ac2aaf60730c37eb486e5d906a
masahiro ueno <ueno@chatwork.co.jp>
2012-11-08 19:02:20 +0900

    add json request header and some error handling.

--- README Preview.py
+++ README Preview.py
@@ -1,6 +1,7 @@
 import sublime
 import sublime_plugin
 import os
+import codecs
 import json
 import urllib2
 import tempfile
@@ -49,18 +50,27 @@ class ReadmePreviewCommand(sublime_plugin.TextCommand):
         try:
             packages = self.readmes.keys()
             package_paths = self.readmes.values()
+            
+            if not os.path.isfile(package_paths[index]):
+                sublime.error_message('ReadMe [Error]: Not found README.')
+                return 
 
-            contents = open(package_paths[index]).read()
-            data = json.dumps({ 'text': contents, 'mode': 'gfm' })
-            html = urllib2.urlopen('https://api.github.com/markdown', data).read().decode('utf-8')
-
+            contents = codecs.open(package_paths[index], encoding='utf8', mode='r').read()
+            data = json.dumps({ 'text': contents, 'mode': 'markdown' })
+            headers = { 'Content-Type': 'application/json' }
+            req = urllib2.Request('https://api.github.com/markdown', data, headers)
+            html = urllib2.urlopen(req).read().decode('utf-8')
+            
             tmp_path = os.path.join(tempfile.gettempdir(), '%s.html' % packages[index])
-            tmp_html = open(tmp_path, 'w')
+            tmp_html = codecs.open(tmp_path, encoding='utf8', mode='w')
             tmp_html.write(build_html(html))
             tmp_html.close()
 
             webbrowser.open('file://' + tmp_path, 1, True)
             sublime.status_message('open %s README file' % packages[index])
-
+            
+        except urllib2.HTTPError as e:
+            sublime.error_message('ReadMe [Error]: HTTPError in Github markdown rendering API.')
+            
         except Exception as e:
             sublime.error_message('ReadMe [Error]: Could not open README.')
--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 README Preview
 ========================
 
+This is a Sublime Text 2 plugin to help you preview already installed other package's README files quickly in you web browser.
+
 ## Usage
 
 ### Command Palette
 
 * via the command palette (**super+shift+p**) and typing
 
-    * choose Package - preview Package's README.
+    * choose Package - preview package's README.
+    
+## Uses
+    
+- [GitHub Markdown Rendering API][0]
+- [markdown.css][1]
+
+[0]: http://developer.github.com/v3/markdown/
+[1]: http://kevinburke.bitbucket.org/markdowncss/
+[2]: https://github.com/astronaughts/README-Preview/
+[3]: http://revolunet.mit-license.org
+
+## Licence
+
+The code is available at github [https://github.com/astronaughts/README-Preview][2] under MIT licence : [http://revolunet.mit-license.org][3]
